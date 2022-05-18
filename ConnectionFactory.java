8685cbb0ea0ce7cabe6d1f854ae1b5f40c603388
Malu Alaquoke <88796071+quokequack@users.noreply.github.com>
2022-05-18 08:44:29 -0300

    Created a connection factory

---
+++ ConnectionFactory.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public static Connection getConnection(){
+        try{
+            return DriverManager.getConnection("jdbc:mysql://localhost/javabd", "root", "");
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
